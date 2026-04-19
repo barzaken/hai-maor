abfd339fa90bfd53cec659f24d0393585895d145
barzaken <bar.zaken@boyar.org.il>
2026-04-19 19:55:32 +0700

    new img

--- components/app/tracks-section.tsx
+++ components/app/tracks-section.tsx
@@ -15,7 +15,7 @@ const tracks = [
 		title: "מפגשים אישיים (VIP)",
 		description: "ליווי אישי מ-A ל-B, כולל צילום וידאו לשיפור תוצאות בלייב ומבחן התוצאה.",
 		featured: true,
-		images: ["/img/tracks/personal.jpeg"],
+		images: ["/img/tracks/personal2.jpg"],
 	},
 	{
 		title: "סדנת פרקטיקום לצוותים",
